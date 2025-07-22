02cde19ce61bc164aedc970ee5ff7d31fb2cb083
uw819kt <miya0arima@gmail.com>
2025-07-22 17:04:59 +0900

    first_commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using cucumber.Models;
+
+namespace cucumber.Data
+{
+    public class AppDbContext
+    {
+    }
+}
---
+++ Models/Enums/ClassificationType.cs
@@ -0,0 +1,12 @@
+﻿namespace cucumber.Models.Enums
+{
+    public enum ClassificationType
+    {
+        FullTime = 0,
+        FourDaysW = 1,
+        ThreeDaysW = 2,
+        TwoDaysW = 3,
+        OneDayW = 4,
+        others = 5
+    }
+}
